4cd48f87bc241898ecf4e5411cf926262c66982a
soyingenieroo <soyingenieroo@gmail.com>
2023-02-18 11:25:28 -0700

    update settings refs

--- GetMyBeatsSettings/asgi.py
+++ GetMyBeatsSettings/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'getmybeats.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GetMyBeatsSettings.settings')
 
 application = get_asgi_application()
