cc58b05a2c87233ee62087541e60a58c1b534697
soyingenieroo <soyingenieroo@gmail.com>
2023-02-18 14:14:51 -0700

    add audio model to admin site

--- GetMyBeatsApp/admin.py
+++ GetMyBeatsApp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.apps import apps
 
+from .models import User, Audio
 
-admin.site.register(User, UserAdmin)
+# reference: https://codinggear.blog/how-to-register-model-in-django-admin
+for model in apps.get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
