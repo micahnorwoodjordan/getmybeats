1478a32e46fca9eb1dd87cca8b66366ac6a3896b
soyingenieroo <soyingenieroo@gmail.com>
2023-02-18 18:02:54 -0700

    util to encode files and commit to db

--- GetMyBeatsApp/models.py
+++ GetMyBeatsApp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.db import IntegrityError, connection, models, transaction
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 from django.utils.timezone import now
+
+from .db.utilities import *
 
 class User(AbstractUser):
     email = models.CharField(max_length=100, blank=False, null=False, unique=True)
@@ -19,11 +21,17 @@ class User(AbstractUser):
 
 class Audio(models.Model):
     id = models.AutoField(primary_key=True)
-    fk_uploaded_by = models.ForeignKey('User', models.DO_NOTHING, null=False, blank=False)
-    uploaded_at = models.DateTimeField(default=now())
+    fk_uploaded_by = models.ForeignKey('User', models.DO_NOTHING, null=False, blank=False, default=User.objects.get(id=1).id)
+    uploaded_at = models.DateTimeField(default=now)
     title = models.CharField(max_length=200, blank=False, null=False, unique=True)
     length = models.CharField(max_length=50, blank=True, null=True)  # TODO: look into django types that might be better to store audio duration data
-    raw_bytes = models.BinaryField(max_length=1000)
+    raw_bytes = models.BinaryField(max_length=1000)  # for big files, some clients will crash trying to retrieve this
+    file_upload = models.FileField()  # specifying `upload_to` will nest the filepath argument. this is not wanted.
+
+    # override save to encode the file
+    def save(self, *args, **kwargs):
+        self.raw_bytes = b64encode_file_upload(self.file_upload)
+        return super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'audio'
