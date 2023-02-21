5e17ffd4c8fa47797cc7a857134b378a471777dc
soyingenieroo <soyingenieroo@gmail.com>
2023-02-20 20:47:35 -0700

    encode file data

--- GetMyBeatsApp/db/utilities.py
+++ GetMyBeatsApp/db/utilities.py
@@ -9,6 +9,5 @@ def b64encode_file_upload(filepath):
     """
     # b64 encode wav files
     # reference: https://stackoverflow.com/questions/30224729/convert-wav-to-base64
-    filepath = f'{settings.MEDIA_ROOT}{filepath}'
     return base64.b64encode(open(filepath, "rb").read())
 
