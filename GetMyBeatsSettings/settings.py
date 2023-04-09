9e0021c32eb09fe94fc9c5094a87c1836d0309da
soyingenieroo <soyingenieroo@gmail.com>
2023-04-09 15:28:25 -0700

    let django serve application domain

--- GetMyBeatsSettings/settings.py
+++ GetMyBeatsSettings/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.getmybeats.com']
 
 
 # Application definition
