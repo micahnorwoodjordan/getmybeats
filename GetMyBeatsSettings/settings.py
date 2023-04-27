8ba662e883503399dbbdad045cb25546a28199ec
soyingenieroo <soyingenieroo@gmail.com>
2023-04-27 15:33:43 -0700

    tweak static and media dirs

--- GetMyBeatsSettings/settings.py
+++ GetMyBeatsSettings/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = '/application/static/'
+STATIC_ROOT = BASE_DIR / '/static/'
 STATIC_URL = '/static/'
 MEDIA_ROOT = '/application//media/'
 MEDIA_URL = '/media/'
