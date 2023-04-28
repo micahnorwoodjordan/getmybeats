51fb2ec5e010832ad087047194649933cafc403e
soyingenieroo <soyingenieroo@gmail.com>
2023-04-27 17:17:07 -0700

    oops

--- GetMyBeatsSettings/settings.py
+++ GetMyBeatsSettings/settings.py
@@ -97,9 +97,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = BASE_DIR / '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/application//media/'
+MEDIA_ROOT = '/application/media/'
 MEDIA_URL = '/media/'
 
 
