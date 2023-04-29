3e5182af6a2d4409d1141f402e4b86b49fbb3929
soyingenieroo <soyingenieroo@gmail.com>
2023-04-28 19:42:58 -0700

    send main audio context to client

--- GetMyBeatsApp/db/utilities.py
+++ GetMyBeatsApp/db/utilities.py
@@ -1,6 +1,7 @@
 import base64
 
-from django.conf import settings
+from GetMyBeatsApp.models import Audio
+from GetMyBeatsApp.serializers import AudioSerializer
 
 
 def b64encode_file_upload(filepath):
@@ -11,3 +12,22 @@ def b64encode_file_upload(filepath):
     # reference: https://stackoverflow.com/questions/30224729/convert-wav-to-base64
     return base64.b64encode(open(filepath, "rb").read())
 
+
+def get_main_audio_context():
+    """
+    the site index requests all Audio objects from the database. this function organizes and filters the
+    data from these object instances for further client-side processing.
+    """
+    all_audio_instances = Audio.objects.all()
+    context = {
+        'filtered_audio': [{status.name: [] for status in Audio.Status}],
+        'all_audio': [dict(audio) for audio in AudioSerializer(all_audio_instances, many=True).data],
+        'statuses': set([Audio.Status(audio.status).name for audio in all_audio_instances])
+    }
+
+    for filter in context['filtered_audio']:
+        for status, song_collection in filter.items():
+            for audio in AudioSerializer(all_audio_instances.filter(status=Audio.Status[status].value), many=True).data:
+                song_collection.append(dict(audio))
+
+    return context
