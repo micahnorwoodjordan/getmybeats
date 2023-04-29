f7b16317d6a60ad665645aeac0320bdc7cab825a
soyingenieroo <soyingenieroo@gmail.com>
2023-04-28 21:12:50 -0700

    status filter dropdown v1; ui enhancements

--- GetMyBeatsApp/static/js/audio/audio-init.js
+++ GetMyBeatsApp/static/js/audio/audio-init.js
@@ -1,10 +1,22 @@
-let buttonElements = document.getElementsByClassName('btn');
+const nonAudioButtonElementIds = ["dropdownMenuButton"];  // dont forget to update array after each new button
+const allButtonElements = document.getElementsByClassName('btn');
+const audioButtonElements = [];
+
+// initialize page by isolating all button elements with an inner Audio element
+window.onload = function() {
+    for (let i = 0; i < allButtonElements.length; i++) {
+        let currentButton = allButtonElements[i];
+        if (!nonAudioButtonElementIds.includes(currentButton.id)) {
+            audioButtonElements.push(currentButton);
+        }
+    }
+}
 
 function playAudio(buttonElement) {
     let targetAudioElement = buttonElement.getElementsByTagName("audio")[0];
 
-    for (let i = 0; i < buttonElements.length; i++) {
-        let audioElement = buttonElements[i].getElementsByTagName("audio")[0];
+    for (let i = 0; i < audioButtonElements.length; i++) {
+        let audioElement = audioButtonElements[i].getElementsByTagName("audio")[0];
         if (audioElement !== targetAudioElement) {
             audioElement.pause();
             audioElement.currentTime = 0;
@@ -16,4 +28,9 @@ function playAudio(buttonElement) {
     } else {
         targetAudioElement.pause();
     }
+}
+
+
+function updateTextWithSelected(selectedDropdownItem) {
+    document.getElementById("dropdownMenuButton").innerHTML = selectedDropdownItem.innerHTML;
 }
