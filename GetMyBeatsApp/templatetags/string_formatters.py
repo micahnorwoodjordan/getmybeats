4217ded888b3c6335504468276de7c28efd3dcfb
soyingenieroo <soyingenieroo@gmail.com>
2023-05-02 19:38:49 -0700

    standardize audio naming conventions; fix s3 write paths

--- GetMyBeatsApp/templatetags/string_formatters.py
+++ GetMyBeatsApp/templatetags/string_formatters.py
@@ -2,20 +2,24 @@ from django import template
 
 register = template.Library()
 
-TITLE_DELIMITER = ' '
-PY_DELIMITER = '_'
-JS_DELIMITER = ''
+SPACE = ' '
+UNDERSCORE = '_'
+NULL_CHARACTER = ''
 
 
 @register.filter
 def strip_charx(string, charx):
-    return string.replace(charx, '')
+    return string.replace(charx, NULL_CHARACTER)
 
 
 @register.filter
 def charx_to_space(string, charx):
-    space = ' '
-    return string.replace(charx, space)
+    return string.replace(charx, SPACE)
+
+
+@register.filter
+def space_to_charx(string, charx):
+    return string.replace(SPACE, charx)
 
 
 @register.filter
@@ -23,7 +27,7 @@ def title_to_py(string):
     """
     Foo Bar -> foo_bar
     """
-    return PY_DELIMITER.join(nugget.lower() for nugget in string.split(TITLE_DELIMITER))
+    return UNDERSCORE.join(nugget.lower() for nugget in string.split(SPACE))
 
 
 @register.filter
@@ -31,7 +35,7 @@ def title_to_js(string):
     """
     Foo Bar -> FooBar
     """
-    return string.replace(TITLE_DELIMITER, JS_DELIMITER)
+    return string.replace(SPACE, NULL_CHARACTER)
 
 
 @register.filter
@@ -39,4 +43,4 @@ def python_to_titleized_js(string):
     """
     foo_bar -> FooBar
     """
-    return JS_DELIMITER.join(nugget.capitalize() for nugget in string.split(PY_DELIMITER))
+    return NULL_CHARACTER.join(nugget.capitalize() for nugget in string.split(UNDERSCORE))
