3dae2466a4bb916da4693c58afc71e5eb77ea588
soyingenieroo <soyingenieroo@gmail.com>
2023-05-17 22:53:03 -0700

    invalidate cache on new uploads and deletes

--- GetMyBeatsApp/db/utilities.py
+++ GetMyBeatsApp/db/utilities.py
@@ -2,9 +2,13 @@ import base64
 
 from django.conf import settings
 from django.core.cache import cache
+from django.utils.timezone import now
 
 from GetMyBeatsApp.models import Audio
 from GetMyBeatsApp.serializers import AudioSerializer
+
+
+NEW_AUDIO_UPLOAD_CACHE_KEY = f'NEW-UPLOAD-{now().strftime("%Y.%m.%d")}'
 
 
 def b64encode_file_upload(filepath):
@@ -18,23 +22,26 @@ def b64encode_file_upload(filepath):
 
 def get_main_audio_context(client_address):
     """
-    request all Audio objects from either the system cache, or the database. this function organizes and filters the
-    data from these object instances for further client-side processing.
+    fetch Audio objects from the site cache or database, and organize object attributes for client-side processing
+    :param client_address: str
+    :return dict
     """
-    all_audio_instances = cache.get(client_address)
-    if all_audio_instances is None:
-        all_audio_instances = Audio.objects.all()
-        cache.set(client_address, all_audio_instances, timeout=settings.AUDIO_CACHE_EXPIRY_SECONDS)
+    fields = ['id', 'fk_uploaded_by', 'uploaded_at', 'title', 'length', 'file_upload', 'status']
+    audios = cache.get(client_address)
+
+    if audios is None:
+        audios = Audio.objects.all().only(*fields)
+        cache.add(client_address, audios, timeout=settings.AUDIO_CACHE_EXPIRY_SECONDS)
 
     context = {
         'filtered_audio': [{status.name: [] for status in Audio.Status}],
-        'all_audio': [dict(audio) for audio in AudioSerializer(all_audio_instances, many=True).data],
-        'statuses': set([Audio.Status(audio.status).name for audio in all_audio_instances])
+        'all_audio': [dict(audio) for audio in AudioSerializer(audios, many=True).data],
+        'statuses': set([Audio.Status(audio.status).name for audio in audios])
     }
 
     for filter in context['filtered_audio']:
         for status, song_collection in filter.items():
-            for audio in AudioSerializer(all_audio_instances.filter(status=Audio.Status[status].value), many=True).data:
+            for audio in AudioSerializer(audios.filter(status=Audio.Status[status].value), many=True).data:
                 song_collection.append(dict(audio))
 
     return context
