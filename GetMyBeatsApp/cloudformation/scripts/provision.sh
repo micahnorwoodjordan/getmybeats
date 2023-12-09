951c9b3a81ae2c8537ae85b0e8357becd6b5f1b5
Micah Norwood <micahnorwoodjordan@gmail.com>
2023-12-08 18:46:35 -0700

    commented annotations; set up logging

--- GetMyBeatsApp/cloudformation/scripts/provision.sh
+++ GetMyBeatsApp/cloudformation/scripts/provision.sh
@@ -1,36 +1,43 @@
 #! /bin/bash
+
+
 export APPLICATION_DIR='/application/getmybeats'
 export NGINX_DIR='/etc/nginx'
+export LOGGING_DIR='/var/log'
 
 
-# echo "installing source code..."	
-# cd $APPLICATION_DIR
-# git clone git@github.com:micahnorwoodjordan/getmybeats.git .
-# git checkout $CODE_VERSION	
-# echo "installation complete."
+# SET UP APPLICATION LOGGING
+echo "beginning logging setup"
+cd $LOGGING_DIR
+mkdir django 
+cd django && touch general.log
+sudo chmod 777 general.log
+echo "logging setup complete"
 
+
+# INSTALL NGINX CONF
 cd $NGINX_DIR && cp $APPLICATION_DIR/GetMyBeatsApp/cloudformation/nginx/nginx.conf nginx.conf
 
 
-# echo "installing dependencies..."
-# cd /application
-# source getmybeatsvenv/bin/activate
-# cd getmybeats && pip3 install -r requirements.txt
-# cp /application/getmybeatsvenv/bin/gunicorn /usr/local/bin
-# deactivate
-# echo "installation complete."
+# INSTALL API DEPENDENCIES
+echo "installing dependencies..."
+cd /application
+source getmybeatsvenv/bin/activate
+cd getmybeats && pip3 install -r requirements.txt
+cp /application/getmybeatsvenv/bin/gunicorn /usr/local/bin
+deactivate
+echo "installation complete."
 
 
+# START SERVICES AND BUILD APPS
 echo "attempting to start gunicorn"
 cd $APPLICATION_DIR
 gunicorn -c ../gunicorn_config.py GetMyBeatsSettings.wsgi --daemon
 echo "successfully started gunicorn."
 
-
 cd $APPLICATION_DIR/frontend
 npm install
 npm run build
-
 
 echo "running management commands..."
 cd $APPLICATION_DIR
@@ -41,4 +48,5 @@ deactivate
 echo "finished running management commands."
 
 
+# DONE
 echo "setup complete."
