7a3fdf190b8e0f94582fae6f658bbd5142ff491d
Micah Norwood <micahnorwoodjordan@gmail.com>
2024-02-28 12:45:45 -0700

    better organize string validation; notes; small cleanup

--- GetMyBeatsApp/models.py
+++ GetMyBeatsApp/models.py
@@ -8,7 +8,10 @@ from django.utils.timezone import now
 from django.conf import settings
 from django.core.cache import cache
 
-from GetMyBeatsApp.templatetags.string_formatters import UNDERSCORE
+from GetMyBeatsApp.templatetags.string_formatters import (
+    PLACEHOLDER_TEXT,
+    validate_s3_path, get_sanitized_title, get_sanitized_s3_path
+)
 
 
 logger = logging.getLogger(__name__)
@@ -49,14 +52,14 @@ class Audio(models.Model):
         image = 2
 
     def populate_s3_upload_path(media_type):
-        """convience method for to populate the s3 fields in the django admin interface"""
+        """convience method to populate the s3 fields in the django admin interface"""
         prefix = 's3://'
-        placeholder_filename = 'XXXXXX'
         if media_type == 1:  # Audio.MediaType.audio
             bucket = settings.S3_AUDIO_BUCKET
         elif media_type == 2:  # Audio.MediaType.image
             bucket = settings.S3_IMAGE_BUCKET
-        return os.path.join(prefix, bucket, placeholder_filename)
+        # explicitly setting placeholder text will fail instance `save` calls if the true file name isn't set by user
+        return os.path.join(prefix, bucket, PLACEHOLDER_TEXT)
 
     id = models.AutoField(primary_key=True)
     fk_uploaded_by = models.ForeignKey('User', models.DO_NOTHING, null=False, blank=False, default=1)  # super user
@@ -73,98 +76,22 @@ class Audio(models.Model):
         max_length=200, blank=False, null=False, unique=True, default=populate_s3_upload_path(MediaType.image.value)
     )
 
-    def get_sanitized_title(string):  # MY NOODLES --> my_noodles
-        # TODO: unit test
-        A = 65
-        a = 97
-        Z = 90
-        z = 122
-        underscore = 95
-        new_title = ''
-        for character in string:
-            is_lowercase = ord(character) >= a and ord(character) <= z
-            is_uppercase = ord(character) >= A and ord(character) <= Z
-            is_letter = is_lowercase or is_uppercase
-            is_underscore = ord(character) == underscore
-            is_space = character == ' '
-
-            if is_letter:
-                new_title += character.lower()
-                continue
-            if is_space:
-                new_title += UNDERSCORE
-                continue
-            if is_underscore:
-                new_title += character
-        return new_title
-
-    def validate_s3_path(path):
-        # TODO: unit test
-        msg = f'invalid s3 path: {path}'
-
-        protocol = 's3://'
-        if protocol not in path:
-            raise Exception(msg)
-
-        ext = os.path.splitext(path)[1]
-        if ext == '':
-            raise Exception(msg)
-
-        basename = os.path.basename(path)
-        bucket = path.replace(protocol, '').replace(basename, '')
-        if bucket == '':
-            raise Exception(msg)
-
-    def get_sanitized_s3_path(path):  # s3://getmybeats-audio-dev/NOODLES.wav --> s3://getmybeats-audio-dev/noodles.wav
-        # TODO: unit test
-        A = 65
-        a = 97
-        Z = 90
-        z = 122
-        underscore = 95
-
-        protocol = 's3://'
-        basename = os.path.basename(path)
-        ext = os.path.splitext(path)[1]
-        bucket = path.replace(protocol, '').replace(basename, '')
-        sanitized = protocol + bucket
-
-        for character in basename:
-            is_lowercase = ord(character) >= a and ord(character) <= z
-            is_uppercase = ord(character) >= A and ord(character) <= Z
-            is_letter = is_lowercase or is_uppercase
-            is_underscore = ord(character) == underscore
-            is_space = character == ' '
-
-            if is_letter:
-                sanitized += character.lower()
-                continue
-            if is_space:
-                sanitized += UNDERSCORE
-                continue
-            if character in ext:
-                sanitized += character
-                continue
-            if is_underscore:
-                sanitized += character
-        return sanitized
-
     def delete(self, *args, **kwargs):
         # NOTE: this is an Audio instance method, meaning that bulk deletes on QuerySets won't invalidate the cache
         cache.clear()
         super(Audio, self).delete()
 
     def save(self, *args, **kwargs):
-        if 'XXXXXX' in self.s3_audio_upload_path:
+        if PLACEHOLDER_TEXT in self.s3_audio_upload_path:
             raise Exception(f'invalid S3 path: {self.s3_audio_upload_path}')
-        if 'XXXXXX' in self.s3_artwork_upload_path:
+        if PLACEHOLDER_TEXT in self.s3_artwork_upload_path:
             raise Exception(f'invalid S3 path: {self.s3_artwork_upload_path}')
 
-        Audio.validate_s3_path(self.s3_audio_upload_path)
-        Audio.validate_s3_path(self.s3_artwork_upload_path)
-        self.title = Audio.get_sanitized_title(self.title)
-        self.s3_audio_upload_path = Audio.get_sanitized_s3_path(self.s3_audio_upload_path)
-        self.s3_artwork_upload_path = Audio.get_sanitized_s3_path(self.s3_artwork_upload_path)
+        validate_s3_path(self.s3_audio_upload_path)
+        validate_s3_path(self.s3_artwork_upload_path)
+        self.title = get_sanitized_title(self.title)
+        self.s3_audio_upload_path = get_sanitized_s3_path(self.s3_audio_upload_path)
+        self.s3_artwork_upload_path = get_sanitized_s3_path(self.s3_artwork_upload_path)
         cache.clear()
         return super().save(*args, **kwargs)
 
--- GetMyBeatsApp/templatetags/string_formatters.py
+++ GetMyBeatsApp/templatetags/string_formatters.py
@@ -1,15 +1,100 @@
+import os
+
 from django import template
+
 
 register = template.Library()
 
+
+# TODO: verify whether all methods are still valid
+# TODO: unit test valid methods
+
+
 SPACE = ' '
 UNDERSCORE = '_'
-NULL_CHARACTER = ''
+NULL = ''
+PLACEHOLDER_TEXT = 'XXXXXX'
+
+
+def validate_s3_path(path):
+    msg = f'invalid s3 path: {path}'
+
+    protocol = 's3://'
+    if protocol not in path:
+        raise Exception(msg)
+
+    ext = os.path.splitext(path)[1]
+    if ext == '':
+        raise Exception(msg)
+
+    basename = os.path.basename(path)
+    bucket = path.replace(protocol, '').replace(basename, '')
+    if bucket == '':
+        raise Exception(msg)
+
+
+def get_sanitized_title(string):  # MY NOODLES --> my_noodles
+    A = 65
+    a = 97
+    Z = 90
+    z = 122
+    underscore = 95
+    new_title = ''
+    for character in string:
+        is_lowercase = ord(character) >= a and ord(character) <= z
+        is_uppercase = ord(character) >= A and ord(character) <= Z
+        is_letter = is_lowercase or is_uppercase
+        is_underscore = ord(character) == underscore
+        is_space = character == ' '
+
+        if is_letter:
+            new_title += character.lower()
+            continue
+        if is_space:
+            new_title += UNDERSCORE
+            continue
+        if is_underscore:
+            new_title += character
+    return new_title
+
+
+def get_sanitized_s3_path(path):  # s3://getmybeats-audio-dev/NOODLES.wav --> s3://getmybeats-audio-dev/noodles.wav
+    A = 65
+    a = 97
+    Z = 90
+    z = 122
+    underscore = 95
+
+    protocol = 's3://'
+    basename = os.path.basename(path)
+    ext = os.path.splitext(path)[1]
+    bucket = path.replace(protocol, '').replace(basename, '')
+    sanitized = protocol + bucket
+
+    for character in basename:
+        is_lowercase = ord(character) >= a and ord(character) <= z
+        is_uppercase = ord(character) >= A and ord(character) <= Z
+        is_letter = is_lowercase or is_uppercase
+        is_underscore = ord(character) == underscore
+        is_space = character == ' '
+
+        if is_letter:
+            sanitized += character.lower()
+            continue
+        if is_space:
+            sanitized += UNDERSCORE
+            continue
+        if character in ext:
+            sanitized += character
+            continue
+        if is_underscore:
+            sanitized += character
+    return sanitized
 
 
 @register.filter
 def strip_charx(string, charx):
-    return string.replace(charx, NULL_CHARACTER)
+    return string.replace(charx, NULL)
 
 
 @register.filter
@@ -27,7 +112,7 @@ def title_to_py(string):
     """
     Foo Bar -> foo_bar
     """
-    return UNDERSCORE.join(nugget.lower() for nugget in string.split(SPACE))
+    return UNDERSCORE.join(token.lower() for token in string.split(SPACE))
 
 
 @register.filter
@@ -35,7 +120,7 @@ def title_to_js(string):
     """
     Foo Bar -> FooBar
     """
-    return string.replace(SPACE, NULL_CHARACTER)
+    return string.replace(SPACE, NULL)
 
 
 @register.filter
@@ -43,4 +128,4 @@ def python_to_titleized_js(string):
     """
     foo_bar -> FooBar
     """
-    return NULL_CHARACTER.join(nugget.capitalize() for nugget in string.split(UNDERSCORE))
+    return NULL.join(token.capitalize() for token in string.split(UNDERSCORE))
