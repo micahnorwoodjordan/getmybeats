c0939bcea52d065e97654697e4945b16f0dab92a
Micah Norwood <micahnorwoodjordan@gmail.com>
2024-06-10 22:28:11 -0700

    move docker stuff to dev dir

--- dev/db/import_db.sh
+++ dev/db/import_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "starting database schema migration"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST < /root/dbmock/db.sql getmybeats_local_old
+mysql -u root -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST < /root/db/db.sql getmybeats_local_old
 echo "database schema migration success"
