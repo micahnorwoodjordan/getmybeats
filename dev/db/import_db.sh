68af315471aa0f73d906b383cb5ace63eaecd459
Micah Norwood <micahnorwoodjordan@gmail.com>
2024-07-01 13:32:56 -0700

    keep dbimport docker service running to avoid mysql already-exists errors

--- dev/db/import_db.sh
+++ dev/db/import_db.sh
@@ -3,3 +3,4 @@
 echo "starting database schema migration"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST < /root/db/db.sql getmybeats_local_old
 echo "database schema migration success"
+tail -f /dev/null
