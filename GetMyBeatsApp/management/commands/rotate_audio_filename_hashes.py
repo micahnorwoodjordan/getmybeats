e4de1e3bb2691b2c118c580685bc7d2629ad010d
Micah Norwood <micahnorwoodjordan@gmail.com>
2024-07-01 19:51:21 -0700

    wrap in db update in transaction

--- GetMyBeatsApp/management/commands/rotate_audio_filename_hashes.py
+++ GetMyBeatsApp/management/commands/rotate_audio_filename_hashes.py
@@ -1,5 +1,6 @@
 import os
 
+from django.db import transaction
 from django.core.management.base import BaseCommand
 
 from GetMyBeatsApp.models import Audio
@@ -11,10 +12,12 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        audios = Audio.objects.all()
-        for audio in audios:
-            filename = os.path.basename(audio.file_upload.path)
-            new_hashed_filename = get_new_hashed_audio_filename(filename)
-            audio.filename_hash = new_hashed_filename
-        updated_count = Audio.objects.bulk_update(audios, ['filename_hash'])
+        updated_count = 0
+        with transaction.atomic():
+            audios = Audio.objects.all()
+            for audio in audios:
+                filename = os.path.basename(audio.file_upload.path)
+                new_hashed_filename = get_new_hashed_audio_filename(filename)
+                audio.filename_hash = new_hashed_filename
+            updated_count = Audio.objects.bulk_update(audios, ['filename_hash'])
         print(updated_count)
