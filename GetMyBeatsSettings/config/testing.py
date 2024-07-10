2311a6e6f439bf1ec37af161df38c48442bb2e76
Micah Norwood <micahnorwoodjordan@gmail.com>
2024-07-10 16:44:58 -0700

    avoid mgmt cmd namespace confusion

---
+++ GetMyBeatsSettings/config/testing.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+from .dev import *
+
+
+DATABASE_SETTINGS = json.loads(os.environ['DATABASE_SETTINGS'])
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': DATABASE_SETTINGS['DBNAME'],
+        'HOST': DATABASE_SETTINGS['DBHOST'],
+        'USER': DATABASE_SETTINGS['DBUSER'],
+        'PASSWORD': DATABASE_SETTINGS['DBPASSWORD'],
+        'PORT': DATABASE_SETTINGS['DBPORT'],
+        'TEST': {
+            'NAME': 'test_getmybeats_local_old'
+        }
+    }
+}
