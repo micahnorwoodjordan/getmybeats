4ca688ece651e2c2f6f5d087074ffd9fad1cc069
Micah Norwood <micahnorwoodjordan@gmail.com>
2024-10-30 16:05:21 -0700

    note to self about mysqldumps in local docker  setup

--- dev/db/import_db.sh
+++ dev/db/import_db.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# example, since i keep forgetting: https://linuxize.com/post/how-to-back-up-and-restore-mysql-databases-with-mysqldump/
+# cd /application/getmybeats
+# mysqldump -u root -p -h db getmybeats_local_old > dev/db/db.sql
+
+# NOTE: the SQL file of a successful db dump will have insert statements for these tables:
+#   `auth_user`
+#   `django_migrations`
+#   `audio`
+#   `audio_fetch_request`
+
 echo "starting database schema migration"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST < /root/db/db.sql getmybeats_local_old
 echo "database schema migration success"
