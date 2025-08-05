9c9199506bb4794c9375184d2144aed6468b51b0
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-08-05 15:26:21 -0700

    oops

--- GetMyBeatsSettings/settings.py
+++ GetMyBeatsSettings/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
