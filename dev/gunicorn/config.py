088f852da19a50456cd4647613494e75092c2d22
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-08-08 19:31:19 -0700

    functional api docker service; update dev gunicorn config

--- dev/gunicorn/config.py
+++ dev/gunicorn/config.py
@@ -1,7 +1,4 @@
-command = '/usr/local/bin/gunicorn'
-pythonpath = '/application/getmybeats'
-workers = 3
+workers = 1
 bind = '0.0.0.0:8000'  # docker network interfaces: https://www.reddit.com/r/docker/comments/hu01ly/comment/fyk4c6q/
 loglevel = "info"
 capture_output = True
-errorlog = "/var/log/gunicorn/error.log"
