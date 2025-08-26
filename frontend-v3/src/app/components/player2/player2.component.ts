3ba2931bc7396d2ca3b40479acb4043f8a5468ba
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-08-26 00:51:43 -0700

    basic ui components replicated

--- frontend-v3/src/app/components/player2/player2.component.ts
+++ frontend-v3/src/app/components/player2/player2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
 import { ApiService } from '../../services/api.service';
 import { Audio2Service } from '../../services/audio2.service';
 
@@ -11,18 +13,31 @@ import { Audio2Service } from '../../services/audio2.service';
 export class Player2Component implements OnInit, OnDestroy {
     constructor(private audio2Service: Audio2Service, private apiService: ApiService) { }
 
-    public getIsPlaying() { return this.audio2Service.isPlaying(); }
-    public getCurrentTime() { return this.audio2Service.currentTime(); }
-    public getDuration() { return this.audio2Service.duration; }
-    private setCurrentTime(newValue: number) { this.currentPlaybackTime = newValue; }
+    public userExperienceReportUrl: string = `${environment.apiHost}/user/experience`
+    public browserSupportsAudioVolumeManipulation: boolean = true;
+
+    public getIsPlaying() { return this.audio2Service.getIsPlaying(); }
+    public getCurrentTime() { return this.audio2Service.getCurrentTime(); }
+    public getDuration() { return this.audio2Service.getDuration(); }
+    public getAudioArtworkImageSrc() { return this.audio2Service.getArtworkImageSrc(); }
+    public getLoading() { return this.audio2Service.getLoading(); }
+    public getDownloadProgress() { return this.audio2Service.getDownloadProgress(); }
+    public getTitle() { return this.audio2Service.getTitle(); }
+    public getShuffleEnabled() { return this.audio2Service.getShuffleEnabled(); }
+    public getRepeatEnabled() { return this.audio2Service.getRepeatEnabled();  }
+
+    private setCurrentPlaybackTime(newValue: number) { this.currentPlaybackTime = newValue; }
 
     public currentPlaybackTime: number = 0;
+    public artworkImage: HTMLImageElement = new Image();
+    public audioHasArtwork: boolean = false;
+    public hasPlaybackError: boolean = false;
     private currentPlaybackTimeRepaintMillis: number = 500;
 
   async ngOnInit() {
     this.audio2Service.getDecryptedAudio();
     setInterval(() => {
-      this.setCurrentTime(this.getCurrentTime());
+      this.setCurrentPlaybackTime(this.getCurrentTime());
     }, this.currentPlaybackTimeRepaintMillis);
 
     // await this.audio2Service.loadFromArrayBuffer(decrypted);
@@ -33,7 +48,7 @@ export class Player2Component implements OnInit, OnDestroy {
   }
 
   togglePlay() {
-    if (this.audio2Service.isPlaying()) {
+    if (this.getIsPlaying()) {
       this.audio2Service.pause();
     } else {
       this.audio2Service.play();
@@ -44,4 +59,36 @@ export class Player2Component implements OnInit, OnDestroy {
     const input = event.target as HTMLInputElement;
     this.audio2Service.seek(parseFloat(input.value));
   }
+
+  onClickShuffle() { }
+  onPrevious() { }
+  onNext() { }
+  onPlayPauseClick() { }
+  onClickRepeat() { }
+  openBottomSheet() { }
+  openCustomSnackBar() { }
+  // ----------------------------------------------------------------------------------------------------------------
+  // feature detection
+  doesBrowserSupportVolumeManipulation() {
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/volume
+    // iOS browsers do NOT allow Javascript to manipulate audio volume
+    // the value of `volume` is always 1
+    // setting a value has no effect on the volume of the media object
+
+    // UPDATE: https://developer.apple.com/documentation/webkitjs/htmlmediaelement/1631549-volume
+    // the latest apple documentation mentions nothing about the below:
+    // `volume` is NOT readonly, meaning that the value of `volume` is actually NOT always 1
+    // this metric cannot be used to perform a better feature detection routine
+    // unfortunately, we have to resort to user agent sniffing, which, though bad practice, will probably suffice in this case
+
+    // how many non-iphone browsers are going to send a user agent header containing the substring "iphone"?
+    if (navigator.userAgent.includes("iPhone")) {
+      return false;
+    }
+    return true;
+  }
+
+  setBrowserSupportsAudioVolumeManipulation(newValue: boolean) { this.browserSupportsAudioVolumeManipulation = newValue; }
+
+// ----------------------------------------------------------------------------------------------------------------
 }
--- frontend-v3/src/app/services/audio2.service.ts
+++ frontend-v3/src/app/services/audio2.service.ts
@@ -18,149 +18,166 @@ constructor(
     private readonly artworkService: ArtworkService,
     private readonly cryptoService: CryptoService
 ) {}
+    private audioContext!: AudioContext;
+    private sourceNode?: AudioBufferSourceNode;
+    private buffer!: AudioBuffer;
+    private startTime = 0;
+    private pausedAt = 0;
+    private rafId?: number;
 
-  private audioContext!: AudioContext;
-  private sourceNode?: AudioBufferSourceNode;
-  private buffer!: AudioBuffer;
-  private startTime = 0;
-  private pausedAt = 0;
+    private isPlaying = signal(false);
+    private currentTime = signal(0);
+    private duration = 0;
+    private loading: boolean = false;
+    private title: string = "null";
+    private shuffleEnabled: boolean = false;
+    private repeatEnabled: boolean = false;
+    private downloadProgress: number = 0;
+    private hasPlaybackError: boolean = false;
+    private artworkImageSrc: string = '';
+    private mediaContext: MediaContextElement[] | undefined = [];  // placeholder for `context` attribute to avoid compilation errors during refactor
 
-  public mediaContext: MediaContextElement[] | undefined = [];  // placeholder for `context` attribute to avoid compilation errors during refactor
+    public getHasPlaybackError() { return this.hasPlaybackError; }
+    public getArtworkImageSrc() { return this.artworkImageSrc; }
+    public getLoading() { return this.loading; }
+    public getDownloadProgress() { return this.downloadProgress; }
+    public getTitle() { return this.title; }
+    public getShuffleEnabled() { return this.shuffleEnabled; }
+    public getIsPlaying() { return this.isPlaying(); }
+    public getRepeatEnabled() { return this.repeatEnabled; }
+    public getCurrentTime() { return this.currentTime(); }
+    public getDuration() { return this.duration; }
 
-  isPlaying = signal(false);
-  currentTime = signal(0);
-  duration = 0;
+    private setHasPlaybackError(newValue: boolean) { this.hasPlaybackError = newValue; }
 
-  private rafId?: number;
-
-  private ensureContext() {
-    if (!this.audioContext) {
-      this.audioContext = new AudioContext();
+    private ensureContext() {
+        if (!this.audioContext) {
+            this.audioContext = new AudioContext();
+        }
     }
-  }
 
-  async loadFromArrayBuffer(arrBuf: ArrayBuffer) {
-    this.ensureContext();
-    await this.decodeBuffer(arrBuf);
-  }
-
-  private async decodeBuffer(arrBuf: ArrayBuffer) {
-    this.buffer = await this.audioContext.decodeAudioData(arrBuf);
-    this.duration = this.buffer.duration;
-  }
-
-  play(offset = this.pausedAt) {
-    if (!this.buffer) return;
-
-    this.stop();
-
-    this.sourceNode = this.audioContext.createBufferSource();
-    this.sourceNode.buffer = this.buffer;
-    this.sourceNode.connect(this.audioContext.destination);
-    this.sourceNode.start(0, offset);
-
-    this.startTime = this.audioContext.currentTime - offset;
-    this.isPlaying.set(true);
-
-    this.trackProgress();
-  }
-
-  pause() {
-    if (!this.isPlaying()) return;
-    this.pausedAt = this.audioContext.currentTime - this.startTime;
-    this.stop();
-  }
-
-  seek(time: number) {
-    this.pausedAt = time;
-    this.currentTime.set(time);
-    if (this.isPlaying()) {
-      this.play(time);
+    async loadFromArrayBuffer(arrBuf: ArrayBuffer) {
+        this.ensureContext();
+        await this.decodeBuffer(arrBuf);
     }
-  }
 
-  stop() {
-    this.isPlaying.set(false);
-    if (this.sourceNode) {
-      this.sourceNode.stop();
-      this.sourceNode.disconnect();
-      this.sourceNode = undefined;
+    private async decodeBuffer(arrBuf: ArrayBuffer) {
+        this.buffer = await this.audioContext.decodeAudioData(arrBuf);
+        this.duration = this.buffer.duration;
     }
-    if (this.rafId) cancelAnimationFrame(this.rafId);
-  }
 
-  private trackProgress() {
-    const update = () => {
-      if (this.isPlaying()) {
-        const elapsed = this.audioContext.currentTime - this.startTime;
-        this.currentTime.set(Math.min(elapsed, this.duration));
-        this.rafId = requestAnimationFrame(update);
-      }
-    };
-    update();
-  }
+    play(offset = this.pausedAt) {
+        if (!this.buffer) return;
 
-  async destroy() {
-    this.stop();
-    if (this.audioContext) {
-      await this.audioContext.close();
+        this.stop();
+
+        this.sourceNode = this.audioContext.createBufferSource();
+        this.sourceNode.buffer = this.buffer;
+        this.sourceNode.connect(this.audioContext.destination);
+        this.sourceNode.start(0, offset);
+
+        this.startTime = this.audioContext.currentTime - offset;
+        this.isPlaying.set(true);
+
+        this.trackProgress();
     }
-  }
 
- async getDecryptedAudio() {
-    // const resp = await fetch('assets/test.mp3');
-    let audioFilenameHash;
-    let mediaContext = await this.getContextSynchronously();
-    if (mediaContext) {
-        // this.setAudioContext(mediaContext);
-        // this.setNumberOfTracks(mediaContext.length);
-        // this.setLoading(true);
-        // this.setDownloadProgress(0);
-        audioFilenameHash = mediaContext[0].audio_filename_hash;
-        // this.setAudioTitle(audioContext[newSelectedAudioIndex].title);
-        // this.setSelectedAudioIndex(newSelectedAudioIndex);
-        this.apiService.downloadAudioTrack(audioFilenameHash, generateAudioRequestGUID()).subscribe(
-            async event => {
-                switch (event.type) {
-                case HttpEventType.DownloadProgress:
-                    // if (event.total !== undefined) {
-                    //     this.setDownloadProgress(Math.round((event.loaded / event.total) * 100));
-                    //     console.log(`getandloadaudiotrack: ${this.downloadProgress}% of data fetched`);
-                    // }
-                    break;
-                case HttpEventType.Response:
-                    console.log(`getandloadaudiotrack: received server response ${event.status}`);
-                    if (event.status == 200) {
-                    if (event.body !== undefined && event.body !== null) {
-                        let encrypted = await event.body.arrayBuffer();
-                        let decrypted = await this.cryptoService.decryptAudioData(encrypted, new Uint8Array([
-                        197, 161, 34, 196, 208, 241, 221, 120,
-                        26, 52, 83, 178, 189, 208, 70, 253,
-                        80, 178, 134, 158, 29, 129, 199, 202,
-                        188, 187, 60, 249, 22, 254, 247, 149
-                        ]));
-                        // this.setLoading(false);
-                        // this.updateAudioOndurationchange();
-                        // if (this.autoplayOnIndexChange) {
-                        this.loadFromArrayBuffer(decrypted);
-                        // }
-                    }
-                    } else {
-                    console.log('getandloadaudiotrack: ERROR fetching audio');
-                    }
-                    break;
-                default:
-                    console.log('getandloadaudiotrack: no response from server yet');
-                }
-            },
-            error => {
-                console.log(`getAndLoadAudioTrack ERROR: ${error.toString()}`);
+    pause() {
+        if (!this.isPlaying()) return;
+        this.pausedAt = this.audioContext.currentTime - this.startTime;
+        this.stop();
+    }
+
+    seek(time: number) {
+        this.pausedAt = time;
+        this.currentTime.set(time);
+        if (this.isPlaying()) {
+            this.play(time);
+        }
+    }
+
+    stop() {
+        this.isPlaying.set(false);
+        if (this.sourceNode) {
+            this.sourceNode.stop();
+            this.sourceNode.disconnect();
+            this.sourceNode = undefined;
+        }
+        if (this.rafId) cancelAnimationFrame(this.rafId);
+    }
+
+    private trackProgress() {
+        const update = () => {
+            if (this.isPlaying()) {
+            const elapsed = this.audioContext.currentTime - this.startTime;
+            this.currentTime.set(Math.min(elapsed, this.duration));
+            this.rafId = requestAnimationFrame(update);
             }
-        )
+        };
+        update();
     }
-    // await this.loadAudioArtworkImage();
-    // return resp.arrayBuffer();
-  }
 
-  public async getContextSynchronously() { return await this.apiService.getMediaContextAsPromise(); }
+    async destroy() {
+        this.stop();
+        if (this.audioContext) {
+            await this.audioContext.close();
+        }
+    }
+
+    async getDecryptedAudio() {
+        // const resp = await fetch('assets/test.mp3');
+        let audioFilenameHash;
+        let mediaContext = await this.getContextSynchronously();
+        if (mediaContext) {
+            // this.setAudioContext(mediaContext);
+            // this.setNumberOfTracks(mediaContext.length);
+            // this.setLoading(true);
+            // this.setDownloadProgress(0);
+            audioFilenameHash = mediaContext[1].audio_filename_hash;
+            // this.setAudioTitle(audioContext[newSelectedAudioIndex].title);
+            // this.setSelectedAudioIndex(newSelectedAudioIndex);
+            this.apiService.downloadAudioTrack(audioFilenameHash, generateAudioRequestGUID()).subscribe(
+                async event => {
+                    switch (event.type) {
+                    case HttpEventType.DownloadProgress:
+                        // if (event.total !== undefined) {
+                        //     this.setDownloadProgress(Math.round((event.loaded / event.total) * 100));
+                        //     console.log(`getandloadaudiotrack: ${this.downloadProgress}% of data fetched`);
+                        // }
+                        break;
+                    case HttpEventType.Response:
+                        console.log(`getandloadaudiotrack: received server response ${event.status}`);
+                        if (event.status == 200) {
+                        if (event.body !== undefined && event.body !== null) {
+                            let encrypted = await event.body.arrayBuffer();
+                            let decrypted = await this.cryptoService.decryptAudioData(encrypted, new Uint8Array([
+                            197, 161, 34, 196, 208, 241, 221, 120,
+                            26, 52, 83, 178, 189, 208, 70, 253,
+                            80, 178, 134, 158, 29, 129, 199, 202,
+                            188, 187, 60, 249, 22, 254, 247, 149
+                            ]));
+                            // this.setLoading(false);
+                            // this.updateAudioOndurationchange();
+                            // if (this.autoplayOnIndexChange) {
+                            this.loadFromArrayBuffer(decrypted);
+                            // }
+                        }
+                        } else {
+                        console.log('getandloadaudiotrack: ERROR fetching audio');
+                        }
+                        break;
+                    default:
+                        console.log('getandloadaudiotrack: no response from server yet');
+                    }
+                },
+                error => {
+                    console.log(`getAndLoadAudioTrack ERROR: ${error.toString()}`);
+                }
+            )
+        }
+        // await this.loadAudioArtworkImage();
+        // return resp.arrayBuffer();
+    }
+
+    public async getContextSynchronously() { return await this.apiService.getMediaContextAsPromise(); }
 }
