8de13b7c86f9afc54118e2931ff57a3389711d21
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-09-22 02:52:03 -0700

    start gunicorn last (last command should be run as a long-running process)

--- GetMyBeatsApp/scripts/start.sh
+++ GetMyBeatsApp/scripts/start.sh
@@ -9,7 +9,6 @@ mkdir media &&
 python manage.py test &&
 python manage.py download_missing_audio &&
 python manage.py rotate_audio_filename_hashes &&
+python manage.py record_production_release &&
+echo "deployment bootstrapping complete. health checks should begin shortly..." &&
 gunicorn --worker-tmp-dir /dev/shm -c GetMyBeatsApp/digitalocean/gunicorn/gunicorn_config.py GetMyBeatsSettings.wsgi &&
-python manage.py record_production_release
-
-echo "deployment bootstrapping complete. health checks should begin shortly..."
