c9381f91ace37087708b7e0ce738909ecbbff380
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-09-22 02:52:20 -0700

    oops

--- GetMyBeatsApp/scripts/start.sh
+++ GetMyBeatsApp/scripts/start.sh
@@ -11,4 +11,4 @@ python manage.py download_missing_audio &&
 python manage.py rotate_audio_filename_hashes &&
 python manage.py record_production_release &&
 echo "deployment bootstrapping complete. health checks should begin shortly..." &&
-gunicorn --worker-tmp-dir /dev/shm -c GetMyBeatsApp/digitalocean/gunicorn/gunicorn_config.py GetMyBeatsSettings.wsgi &&
+gunicorn --worker-tmp-dir /dev/shm -c GetMyBeatsApp/digitalocean/gunicorn/gunicorn_config.py GetMyBeatsSettings.wsgi
