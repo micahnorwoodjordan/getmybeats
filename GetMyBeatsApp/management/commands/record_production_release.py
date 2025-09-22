f6f9f3e7535c615cbd887112cdee757d74b45d17
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-09-22 02:12:41 -0700

    add log calls to mgmt commands

--- GetMyBeatsApp/management/commands/record_production_release.py
+++ GetMyBeatsApp/management/commands/record_production_release.py
@@ -2,8 +2,11 @@ import os
 
 from django.core.management.base import BaseCommand
 
-from GetMyBeatsApp.models import ProductionRelease
-from GetMyBeatsApp.services.digital_ocean_service import DigitalOceanService
+from GetMyBeatsApp.models import ProductionRelease, LogEntry
+from GetMyBeatsApp.services.log_service import LogService
+
+
+MODULE = __name__
 
 
 class Command(BaseCommand):
@@ -13,13 +16,7 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        env = dict()
-        wanted = ['CODE_BRANCH', 'S3_RELEASE_PROVISIONING_SCRIPT_NAME']  # only these params are prone to change. all other are basically the same
-        for k, v in os.environ.items():
-            if k in wanted:
-                env[k] = v
-
-        hostname = DigitalOceanService()._get_droplet_hostname()
-        env['HOSTNAME'] = hostname  # not truly an env var, but it IS persistent for the lifetime of the app instance
-
-        ProductionRelease.objects.create(environment=env)
+        try:
+            ProductionRelease.objects.create(environment={k: v for k, v in os.environ.items()})
+        except Exception as e:
+            LogService.log(LogEntry.LogLevel.ERROR, f'error recording production release: {e}', MODULE)
