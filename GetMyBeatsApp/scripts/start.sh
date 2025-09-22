c5ed5a6d8b983d3342510234513d15a00b728732
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-09-22 02:23:12 -0700

    execute each command sequentially

--- GetMyBeatsApp/scripts/start.sh
+++ GetMyBeatsApp/scripts/start.sh
@@ -1,9 +1,9 @@
 set -e
 
 
-mkdir media
-python manage.py test
-python manage.py download_missing_audio
-python manage.py rotate_audio_filename_hashes
-gunicorn --worker-tmp-dir /dev/shm -c GetMyBeatsApp/digitalocean/gunicorn/gunicorn_config.py GetMyBeatsSettings.wsgi
+mkdir media &&
+python manage.py test &&
+python manage.py download_missing_audio &&
+python manage.py rotate_audio_filename_hashes &&
+gunicorn --worker-tmp-dir /dev/shm -c GetMyBeatsApp/digitalocean/gunicorn/gunicorn_config.py GetMyBeatsSettings.wsgi &&
 python manage.py record_production_release
