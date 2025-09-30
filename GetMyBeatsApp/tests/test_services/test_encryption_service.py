597ea81e9d7ab1b539e52b5e6e1fa9c9b8b274bf
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-09-29 22:24:03 -0700

    add encryption service test module

---
+++ GetMyBeatsApp/tests/test_services/test_encryption_service.py
@@ -0,0 +1,26 @@
+import uuid
+import random
+import pytest
+
+from django.conf import settings
+
+from GetMyBeatsApp.models import LogEntry, AudioFetchRequest
+
+from GetMyBeatsApp.services.encryption_service import EncryptionService
+
+
+def generate_encryption_key_payload() -> dict:
+    # simulate a 256-bit, ASCII-encoded encryption key dict
+    payload = {str(i): random.randint(0, 255) for i in range(32)}
+    assert len(bytes(payload.values())) == 32
+    return payload
+
+
+def test_process_new_key():
+    initial_log_entry_count = LogEntry.objects.filter().count()
+    initial_audio_fetch_request_count = AudioFetchRequest.objects.filter().count()
+    encoded_key = generate_encryption_key_payload()
+    service = EncryptionService()
+    service.process_new_key(str(uuid.uuid4()), encoded_key)
+    assert LogEntry.objects.filter().count() == initial_log_entry_count + 1
+    assert AudioFetchRequest.objects.filter().count() == initial_audio_fetch_request_count + 1
