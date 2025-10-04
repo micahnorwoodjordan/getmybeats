0c9ccbf32d25e622428856340d0ea70383894cc6
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-10-04 16:33:28 -0700

    undo comment out

--- GetMyBeatsApp/scripts/start.sh
+++ GetMyBeatsApp/scripts/start.sh
@@ -7,9 +7,9 @@ set -e  # fail on 1st error
 
 mkdir media &&
 python manage.py test --settings=GetMyBeatsSettings.settings_test &&
-# python manage.py migrate GetMyBeatsApp &&
-# python manage.py download_missing_audio &&
-# python manage.py rotate_audio_filename_hashes &&
-# python manage.py record_production_release &&
-# echo "deployment bootstrapping complete. health checks should begin shortly..." &&
+python manage.py migrate GetMyBeatsApp &&
+python manage.py download_missing_audio &&
+python manage.py rotate_audio_filename_hashes &&
+python manage.py record_production_release &&
+echo "deployment bootstrapping complete. health checks should begin shortly..." &&
 gunicorn --worker-tmp-dir /dev/shm -c GetMyBeatsApp/digitalocean/gunicorn/gunicorn_config.py GetMyBeatsSettings.wsgi
