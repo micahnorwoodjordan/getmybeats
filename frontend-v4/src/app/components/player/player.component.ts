6980e5d16152f00e6f839696ec99e829ddb2d844
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-04-25 12:58:58 -0700

    skeleton for trackmedia component

--- frontend-v4/src/app/components/player/player.component.ts
+++ frontend-v4/src/app/components/player/player.component.ts
@@ -3,6 +3,7 @@ import { PrimaryComponent } from '../controls/primary/primary.component';
 import { SecondaryComponent } from '../controls/secondary/secondary.component';
 import { ProgressComponent } from '../controls/progress/progress.component';
 import { DetailsComponent } from '../details/details.component';
+import { TrackMediaComponent } from '../trackmedia/trackmedia.component';
 
 
 @Component({
@@ -11,11 +12,12 @@ import { DetailsComponent } from '../details/details.component';
     PrimaryComponent,
     SecondaryComponent,
     ProgressComponent,
-    DetailsComponent
+    DetailsComponent,
+    TrackMediaComponent
   ],
   templateUrl: './player.component.html',
   styleUrl: './player.component.css',
 })
 export class PlayerComponent {
-
+  public albumArtUrl: string = 'https://static.micahnorwoodjordan.com/me-sitting.png';  // TODO
 }
