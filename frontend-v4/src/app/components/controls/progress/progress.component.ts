75a651740c09f3af24d1568824e0ad90dee8e6ff
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-04-25 11:53:23 -0700

    skeleton for progresscomponent

---
+++ frontend-v4/src/app/components/controls/progress/progress.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+
+
+@Component({
+  selector: 'app-progress',
+  imports: [
+    MatIconModule,
+    MatSliderModule
+  ],
+  templateUrl: './progress.component.html',
+  styleUrl: './progress.component.css'
+})
+export class ProgressComponent {  // TODO
+  public durationHumanReadable: string = "3:23";
+  public currentTimeHumanReadable: string = "1:23";
+  public currentTime: string = "1:56";
+  public duration: number = 50;
+
+  public onSeek(event: Event) {  }
+}
--- frontend-v4/src/app/components/player/player.component.ts
+++ frontend-v4/src/app/components/player/player.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { PrimaryComponent } from '../controls/primary/primary.component';
 import { SecondaryComponent } from '../controls/secondary/secondary.component';
+import { ProgressComponent } from '../controls/progress/progress.component';
 
 
 @Component({
   selector: 'app-player',
   imports: [
     PrimaryComponent,
-    SecondaryComponent
+    SecondaryComponent,
+    ProgressComponent
   ],
   templateUrl: './player.component.html',
   styleUrl: './player.component.css',
