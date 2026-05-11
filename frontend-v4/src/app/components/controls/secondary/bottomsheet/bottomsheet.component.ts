c55ca92c7c9a48149b4148bd36776af751fdcbd4
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-05-11 11:38:39 -0700

    fire bottomsheet on icon click

---
+++ frontend-v4/src/app/components/controls/secondary/bottomsheet/bottomsheet.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+
+import { MediaContextElement } from '../../../../interfaces/MediaContextElement';
+
+@Component({
+  selector: 'app-queue-sheet',
+  standalone: true,
+  templateUrl: './bottomsheet.component.html',
+  styleUrl: './bottomsheet.component.css',
+  imports: [MatListModule]
+})
+export class BottomSheetComponent {
+constructor(
+    private bottomSheetRef: MatBottomSheetRef<BottomSheetComponent>,
+
+    @Inject(MAT_BOTTOM_SHEET_DATA)
+    public mediaContext: MediaContextElement[]
+  ) { }
+}
--- frontend-v4/src/app/components/controls/secondary/secondary.component.ts
+++ frontend-v4/src/app/components/controls/secondary/secondary.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 
+import { BottomSheetComponent } from './bottomsheet/bottomsheet.component';
 import { environment } from '../../../../environments/environment.dev';
+import { MediaContextElement } from '../../../interfaces/MediaContextElement';
 
 
 @Component({
@@ -13,10 +16,21 @@ import { environment } from '../../../../environments/environment.dev';
   styleUrl: './secondary.component.css',
 })
 export class SecondaryComponent {
+  constructor(private bottomSheet: MatBottomSheet) { }
+
+  @Input() mediaContext: MediaContextElement[] = [];
+  @Input() isLoading: boolean = true;
+
   public userExperienceReportUrl: string = environment.apiHost + "/user/experience";
   public browserSupportsAudioVolumeManipulation: boolean = true;
 
-  public openBottomSheet() {  }
-  public openCustomSnackBar() {  }
-  public getIsLoading() {  }
+  public openBottomSheet() {
+    this.bottomSheet.open(BottomSheetComponent, {
+      data: this.mediaContext
+    });
+  }
+
+  public closeBottomSheet() { this.bottomSheet.dismiss(); }  // TODO
+
+  public openCustomSnackBar() {  } // TODO
 }
